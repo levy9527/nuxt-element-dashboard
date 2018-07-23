b15446ba35ecb5a9bc5547b83b3dda1c34a83456
Edgar <15989069487@163.com>
2018-07-24 00:30:44 +0800

    使用谷歌内嵌浏览器框架的浏览器（如360，搜狗），使用谷歌内核渲染

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,7 @@ module.exports = {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {'http-equiv': 'x-ua-compatible', content: 'IE=edge'},
+      {'http-equiv': 'x-ua-compatible', content: 'IE=edge, chrome=1'},
       {
         hid: 'description',
         name: 'description',
